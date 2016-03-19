46f7ad38b2d4a489f042cc28f89ec267d948d483
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2016-03-19 16:56:24 +0100

    Support strings in the erlang term format.

--- eterm.ml
+++ eterm.ml
@@ -2,35 +2,58 @@ open Core.Std
 
 exception Parse_error of string * int
 exception Encode_error of string
-                            
-type t =
-  | ET_Int of Int.t
-  | ET_Atom of String.t
+exception Invariant
+            
+type t = ET_Int of Int.t
+       | ET_Atom of String.t
+       | ET_String of String.t
 
-let decode rbyte rint _rstr _rbuf =
+let decode rbyte rint rstr _rbuf =
   match rbyte() with
   (* INTEGERS *)
-  | 97 -> ET_Int (rbyte ())
-  | 98 -> ET_Int (rint ())
+  | 97 ->
+     ET_Int (rbyte ())
+  | 98 ->
+     ET_Int (rint ())
+  | (100 | 107) as c ->
+     let len2 = rbyte () in
+     let len1 = rbyte() in
+     let len = 256 * len2 + len1 in
+     let s = rstr len in
+       (match c with
+        | 100 -> ET_Atom s
+        | 107 -> ET_String s
+        | _ -> raise Invariant)
   | n -> raise (Parse_error ("unknown term tag", n))
 
 let encode wbyte wint wstr _wbuf term =
   match term with
-  | ET_Int n when n >= 0 && n < 256 ->
-     wbyte 97;
-     wbyte n
-  | ET_Int n ->
-     wbyte 98;
-     wint n
+  | ET_Int n when n >= 0 && n < 256 -> wbyte 97; wbyte n
+  | ET_Int n -> wbyte 98; wint n
   | ET_Atom str ->
-     match String.length str with
-     | len when len < 256 ->
-        wbyte 100;
-        wbyte 0;
-        wbyte len;
-        wstr str
-     | _ ->
-        raise (Encode_error "Atom length exceeds 256 bytes")
+     (match String.length str with
+      | len when len < 256 ->
+         wbyte 100;
+         wbyte 0;
+         wbyte len;
+         wstr str
+      | _ ->
+         raise (Encode_error "Atom length exceeds 256 bytes"))
+  | ET_String str ->
+     (match String.length str with
+      | len when len < 65536 ->
+          let a,b = len / 256, len mod 256 in
+          wbyte 107;
+          wbyte a;
+          wbyte b;
+          wstr str
+      | len ->
+         wbyte 108;
+         wint len;
+         (* Output a list of bytes to the Erlang world *)
+         String.iter str
+            ~f:(fun c -> wbyte 97; wbyte (int_of_char c));
+         wbyte 106)
 
 (* The buffer code here is used to read and write terms from buffers *)
 (* as an intermediary point of the data. It is not perfect, but it is *)
@@ -76,7 +99,6 @@ module Buffer = struct
 end
                   
 let of_buffer = Buffer.read
-    
 let to_buffer = Buffer.write
       
   
